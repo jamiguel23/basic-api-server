dbd2d781e7a196d735220e8c1a059eb7375f2fe8
jamiguel23 <josematthewmiguel@gmail.com>
2022-01-12 21:59:39 -0800

    added PUT and DELETE routes. Testing for delete works but not update.

--- __test__/food.test.js
+++ __test__/food.test.js
@@ -46,7 +46,7 @@ xdescribe('testing creating food', () => {
 
 });
 
-describe('testing getting one food', () => {
+xdescribe('testing getting one food', () => {
 
   it('should read form food data', async () => {
 
@@ -54,6 +54,28 @@ describe('testing getting one food', () => {
 
     expect(response.status).toEqual(200);
     // expect(response.body.id).toBeDefined();
+  });
+
+});
+
+describe('testing PUT', () => {
+
+  it('should respond with a 200 with updating a record using PUT', async () => {
+    const response = await request.put('/food/2').send(foodData);
+    expect(response.status).toBe(200);
+    expect(response.body.food).toEqual('Hot Dog');
+  });
+
+});
+
+xdescribe('testing DELETE', () => {
+
+  it('should respond with a 200 with removing a record using DELETE', async () => {
+    const response = await request.delete('/food/1');
+    expect(response.status).toBe(200);
+
+    const result = await request.get('/food/1');
+    expect(result.body).toEqual(null);
   });
 
 });
